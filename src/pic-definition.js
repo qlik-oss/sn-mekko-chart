c75e2420320fa55a45359fd9c10bd930dd081e07
Miralem Drek <mek@qlik.com>
2019-07-08 10:50:48 +0200

    feat: add top level labels

--- src/pic-definition.js
+++ src/pic-definition.js
@@ -154,6 +154,54 @@ export default function ({
           },
         }],
       },
+    }, {
+      key: 'span-boxes',
+      type: 'box',
+      dock: 'top',
+      preferredSize: () => 32,
+      data: {
+        collection: 'span',
+      },
+      settings: {
+        major: {
+          binStart: {
+            scale: 'm',
+            fn(d) {
+              const ss = d.resources.scale('b');
+              return d.resources.scale('m')(ss.datum(d.datum.value).start.value);
+            },
+          },
+          binEnd: {
+            fn(d) {
+              const ss = d.resources.scale('b');
+              return d.resources.scale('m')(ss.datum(d.datum.value).end.value);
+            },
+          },
+        },
+        minor: { start: 0, end: 1 },
+        box: {
+          fill: 'rgba(100, 0, 0, 0.0)',
+        },
+      },
+    }, {
+      type: 'labels',
+      dock: '@span-boxes',
+      displayOrder: 2,
+      settings: {
+        sources: [{
+          component: 'span-boxes',
+          selector: 'rect',
+          strategy: {
+            type: 'rows',
+            settings: {
+              fill: '#111',
+              labels: [{
+                label: d => (d.data ? d.data.series.label : ''),
+              }],
+            },
+          },
+        }],
+      },
     }],
   };
 }

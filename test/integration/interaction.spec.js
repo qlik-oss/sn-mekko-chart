ce02fab08f2ed9c9a48101ad3af828136f6275fe
Andreas Jonsson <aoj@qlik.com>
2019-09-20 09:11:40 +0200

    test: add test for tooltip (#22)
    * test: add test for tooltip
    
    * test: change to beforeEach

--- test/integration/interaction.spec.js
+++ test/integration/interaction.spec.js
@@ -1,11 +1,13 @@
 describe('interaction', () => {
   const content = '.nebulajs-sn';
-  it('should select "Americas, Europe" and "2012"', async () => {
-    const app = encodeURIComponent(process.env.APP_ID || '/apps/Executive_Dashboard.qvf');
-    await page.goto(`${process.testServer.url}/render/app/${app}?cols=Region,Fiscal Year,=1&permissions=interact,select`);
+  const app = encodeURIComponent(process.env.APP_ID || '/apps/Executive_Dashboard.qvf');
+  beforeEach(async () => {
+    await page.goto(`${process.testServer.url}/render/app/${app}?cols=Region,Fiscal Year,=1&permissions=interact,select,passive`);
     await page.waitForSelector(content, {
       timeout: 5000,
     });
+  });
+  it('should select "Americas, Europe" and "2012"', async () => {
     await page.click('[data-key="column-boxes"] rect[data-label="Americas"]');
     await page.click('[data-key="column-boxes"] rect[data-label="Europe"]');
     await page.click('button[title="Confirm selection"]');
@@ -18,5 +20,24 @@ describe('interaction', () => {
 
     rects = await page.$$eval('[data-key="cells"] rect[data-label]', sel => sel.map(r => r.getAttribute('data-label')));
     expect(rects).to.eql(['2012', '2012']);
+  });
+  it('should show tooltip for column "Europe" and data cell "Americas 2012"', async () => {
+    // hover column "Europe"
+    await page.hover('[data-key="column-boxes"] rect[data-label="Europe"]');
+    await page.waitForSelector('.pic-tooltip', { visible: true });
+    let tooltipContent = [];
+    let tooltiphHeader = await page.$eval('.pic-tooltip-content th', header => header.textContent);
+    let tooltipValue = await page.$eval('.pic-tooltip-content tr', value => value.textContent);
+    tooltipContent.push(tooltiphHeader, tooltipValue);
+    expect(tooltipContent).to.eql(['Europe', '=1:3']);
+    // hover "Americas, 2012"
+    tooltipContent = [];
+    const rects = await page.$$('[data-key="cells"] rect[data-label="2012"]');
+    await rects[1].hover();
+    await page.waitForSelector('.pic-tooltip', { visible: true });
+    tooltiphHeader = await page.$eval('.pic-tooltip-content th', header => header.textContent);
+    tooltipValue = await page.$eval('.pic-tooltip-content tr', value => value.textContent);
+    tooltipContent.push(tooltiphHeader, tooltipValue);
+    expect(tooltipContent).to.eql(['Americas, 2012', '=1:33.33%']);
   });
 });

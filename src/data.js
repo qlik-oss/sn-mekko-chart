189285f87bf2714ed7a741f0c1b761c11d8d84d6
Elise Eborn <fdq@qlik.com>
2020-05-25 14:03:05 +0200

    feat: dimension and measure description

--- src/data.js
+++ src/data.js
@@ -1,15 +1,27 @@
-export default {
+export default ({ flags, translator }) => ({
   targets: [
     {
       path: '/qHyperCubeDef',
       dimensions: {
         min: 2,
         max: 2,
+        description: (properties, index) => {
+          if (flags && flags.isEnabled('REQ_LABELS')) {
+            return index === 0
+            ? translator.get('Visualizations.Descriptions.Column')
+            : translator.get('Visualizations.Descriptions.Cells');
+          }
+        },
       },
       measures: {
         min: 1,
         max: 1,
+        description: () => {
+          if (flags && flags.isEnabled('REQ_LABELS')) {
+            return translator.get('Visualizations.Descriptions.Size');
+          }
+        },
       },
     },
   ],
-};
+});

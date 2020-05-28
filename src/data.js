9dff53ce5ffa8e2e727a5383a8da29e923909b58
Elise <fdq@qlik.com>
2020-05-28 15:59:24 +0200

    fix: add dimension and measure description (#57)

--- src/data.js
+++ src/data.js
@@ -1,15 +1,23 @@
-export default {
+export default ({ translator }) => ({
   targets: [
     {
       path: '/qHyperCubeDef',
       dimensions: {
         min: 2,
         max: 2,
+        description: (properties, index) => {
+          return index === 0
+            ? translator.get('Visualizations.Descriptions.Column')
+            : translator.get('Visualizations.Descriptions.Cells');
+        },
       },
       measures: {
         min: 1,
         max: 1,
+        description: () => {
+          return translator.get('Visualizations.Descriptions.Size');
+        },
       },
     },
   ],
-};
+});

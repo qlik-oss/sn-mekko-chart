ffe2752e853baf189c2187ddcc11bf1e1b8bc3b5
veinfors <cvs@qlik.com>
2021-10-05 14:36:22 +0200

    docs: set api spec to stable

--- spec-configs/props.conf.js
+++ spec-configs/props.conf.js
@@ -12,14 +12,10 @@ if (!v) {
 const pkg = require(path.resolve(__dirname, '../package.json')); // eslint-disable-line
 
 module.exports = {
-  glob: [
-    './src/object-properties.js',
-    './src/coloring/index.js',
-    './src/coloring/byDimension.js',
-  ],
+  glob: ['./src/object-properties.js', './src/coloring/index.js', './src/coloring/byDimension.js'],
   package: path.resolve(__dirname, '../package.json'),
   api: {
-    stability: 'experimental',
+    stability: 'stable',
     properties: {
       'x-qlik-visibility': 'public',
     },

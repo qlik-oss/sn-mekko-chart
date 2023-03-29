81ebd45e1584a68a82469c0b950fc7cd1f24a6c1
Christofer Bengtsson <cbt@qlik.com>
2023-03-29 14:30:36 +0200

    chore: fix lint issues

--- src/data.js
+++ src/data.js
@@ -5,16 +5,12 @@ export default ({ translator }) => ({
       dimensions: {
         min: 2,
         max: 2,
-        description: (properties, index) => {
-          return index === 0 ? translator.get('Column') : translator.get('Cells');
-        },
+        description: (properties, index) => (index === 0 ? translator.get('Column') : translator.get('Cells')),
       },
       measures: {
         min: 1,
         max: 1,
-        description: () => {
-          return translator.get('Size');
-        },
+        description: () => translator.get('Size'),
       },
     },
   ],
